e5c3827e6e4b35f10da9ed81ae3a639a06575b9d
Przemek Juzwiszin <przemekjuzwiszin@gmail.com>
2021-02-02 18:28:02 +0100

    feat: add coments to JS code

--- src/js/main.js
+++ src/js/main.js
@@ -16,23 +16,44 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+//Prosty edytor tekstu, który zapisuje wiadomość
+//... po naciśnięciu w przycisk save.
+//Po odświeżeniu przeglądarki i naciśnięciu przycisku load
+//... tekst zapisany w local storage zostaje umieszczony 
+//... w polu textarea.
+
+
+//Pobieranie elementu za pomocą queryselectora
 const formMessage = document.querySelector('.form__message--js');
 console.log(formMessage);
 
+//Pobieranie elementu za pomocą queryselectora
 const saveMessage = document.querySelector('.form__button--save-js');
 console.log(saveMessage);
 
-
+//Pobieranie elementu za pomocą queryselectora
 const loadMessage = document.querySelector('.form__button--load-js');
 console.log(loadMessage);
 
 saveMessage.addEventListener('click', (e) => {
+  
+  //Zapobiega odświeżeniu przeglądarki po naciśnięciu buttona.
   e.preventDefault();
-  const formMessage = document.querySelector('.form__message--js');
+
+  //Zapisuje w localStorage klucz message i wartość textarea.
   localStorage.setItem('message', formMessage.value);
 });
 
+
 loadMessage.addEventListener('click', (e) => {
-e.preventDefault();
+
+//Zapobiega odświeżeniu przeglądarki po naciśnięciu buttona
+//..buttona przestaje wysyłać formularz metodą GET.
+//Strona nie będzie się przeładowywać po naciśnięciu buttona.
+//Blokuje domyślne zachowanie buttona czyli wysyłanie formularza.
+  e.preventDefault();
+
+//Pobiera wartość z localStorage o kluczu message i umieszcza ją 
+//... w polu textarea.   
 formMessage.value = localStorage.getItem('message');
 });
